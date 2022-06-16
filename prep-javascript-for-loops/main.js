3d26d69e51cfed5c2379ace68532dee53377503d
Branden Jones <branden.wayne.jones@gmail.com>
2022-06-16 23:21:16 +0000

    Branden Jones - Completed JS For Loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,21 @@
+var a = 'Exercise #1';
+console.log(a);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+var b = 'Exercise #2';
+console.log(b);
+
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+var c = 'Exercise #3';
+console.log(c);
+
+for (let i = 101; i--;) {
+  console.log('Time Till The Toilet Bomb Explodes:' + (i) + '!');
+  if (i === 0) {
+    console.log('BOOM!!!!!');
+  }
+}
